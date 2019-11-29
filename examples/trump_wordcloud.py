71738e167e99c843f585ea2a9cf153d4adcf9bc1
TimLC <lechateliert@gmail.com>
2019-11-29 15:39:22 +0100

    code

---
+++ examples/trump_wordcloud.py
@@ -0,0 +1,44 @@
+from os import path
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+d = path.dirname(__file__)
+
+trump = open(path.join(d, '../speeches.txt'), encoding="utf-8").read()
+
+import numpy as np
+
+
+corpus = trump.split ()
+
+
+def make_pairs(corpus):
+    for i in range (len (corpus) - 1):
+        yield (corpus[i], corpus[i + 1])
+
+
+pairs = make_pairs (corpus)
+
+word_dict = {}
+
+for word_1, word_2 in pairs:
+    if word_1 in word_dict.keys ():
+        word_dict[word_1].append (word_2)
+    else:
+        word_dict[word_1] = [word_2]
+
+first_word = np.random.choice (corpus)
+
+while first_word.islower ():
+    first_word = np.random.choice (corpus)
+
+chain = [first_word]
+
+n_words = 50
+
+for i in range (n_words):
+    chain.append (np.random.choice (word_dict[chain[-1]]))
+
+' '.join (chain)
+
+print(chain)
